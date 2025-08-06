96d3dc6a87359f1946812b0daee830ee03305e25
z0w1non <z0w1non@gmail.com>
2025-08-06 20:21:52 +0900

    Update main.cpp

--- llmcpp/main.cpp
+++ llmcpp/main.cpp
@@ -63,7 +63,7 @@ class command_line_syntax_exception : public base_exception {};
 namespace error_info
 {
     using stacktrace = boost::error_info<struct tag_stacktrace, boost::stacktrace::stacktrace>;
-    using description = boost::error_info<struct description_tag, std::string>;
+    using description = boost::error_info<struct tag_description, std::string>;
     using path = boost::error_info<struct tag_file_path, std::filesystem::path>;
     using string = boost::error_info<struct tag_string, std::string>;
 
